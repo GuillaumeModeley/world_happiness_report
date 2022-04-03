d059d01744cef42d323f5565bf784393edf54f6d
guillaume <gui.modeley@gmail.com>
2022-04-03 14:46:06 +0400

    Added regions table and image_url
    create_db.py
    	-Added regions table
    	-Added image_url
    
    extract_data.py
    	-reformated "INSERT INTO countries" statement to be more readable
    	-extract region information
    	-extract image_url

--- create_db.py
+++ create_db.py
@@ -6,13 +6,24 @@ connection = sqlite3.connect("out/happiness.db")
 
 cursor = connection.cursor()
 
+cursor.execute("PRAGMA foreign_keys=ON")
+
+cursor.execute('''CREATE TABLE regions (
+                      region_id INTEGER PRIMARY KEY,
+                      region_name TEXT UNIQUE,
+                      region_code INTEGER UNIQUE
+               )''')
+
 cursor.execute('''CREATE TABLE countries (
                       country_id INTEGER PRIMARY KEY,
                       country_name TEXT NOT NULL,
-                      country_code INTEGER
+                      country_code INTEGER,
+                      image_url TEXT,
+                      region_id INTEGER,
+                      FOREIGN KEY (region_id)
+                          REFERENCES regions (region_id)
                )''')
 
 connection.commit()
 
 connection.close()
-
--- extract_data.py
+++ extract_data.py
@@ -7,10 +7,31 @@ connection = sqlite3.connect('out/happiness.db')
 
 cursor = connection.cursor()
 
+cursor.execute("PRAGMA foreign_keys=ON")
+
 with open('data/countries_continents_codes_flags_url.json', 'r') as f:
     for country_data in json.load(f):
-        cursor.execute('INSERT INTO countries (country_name, country_code) VALUES (?, ?)', (country_data['country'], country_data['country-code']))
+        cursor.execute('''INSERT OR IGNORE INTO regions
+                              (region_name,
+                               region_code)
+                          VALUES
+                              (?, ?)''',
+                       (country_data['region'],
+                        country_data['region-code']))
+
+        cursor.execute('''INSERT INTO countries
+                              (country_name,
+                               country_code,
+                               image_url,
+                               region_id)
+                          VALUES
+                              (?, ?, ?,
+                              (SELECT region_id FROM regions WHERE region_name = ?)
+                              )''',
+                       (country_data['country'],
+                        country_data['country-code'],
+                        country_data['image_url'],
+                        country_data['region']))
 
 connection.commit()
 connection.close()
-
