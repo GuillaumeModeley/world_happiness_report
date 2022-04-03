e4768cf6bb783daa65d925c06f375b8ffa8f67cd
guillaume <gui.modeley@gmail.com>
2022-04-03 17:09:05 +0400

    create happiness_report table
    	-refactor: put countries info extraction in a function
    	-refactor: put database creation code in a function

--- create_db.py
+++ create_db.py
@@ -2,28 +2,42 @@
 
 import sqlite3
 
-connection = sqlite3.connect("out/happiness.db")
+def create_database(db_path):
+    '''Create a database to store the World Happiness Report data'''
 
-cursor = connection.cursor()
+    connection = sqlite3.connect(db_path)
 
-cursor.execute("PRAGMA foreign_keys=ON")
+    cursor = connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
 
-cursor.execute('''CREATE TABLE regions (
-                      region_id INTEGER PRIMARY KEY,
-                      region_name TEXT UNIQUE,
-                      region_code INTEGER UNIQUE
-               )''')
+    cursor.execute('''CREATE TABLE regions (
+                          region_id INTEGER PRIMARY KEY,
+                          region_name TEXT UNIQUE,
+                          region_code INTEGER UNIQUE
+                   )''')
 
-cursor.execute('''CREATE TABLE countries (
-                      country_id INTEGER PRIMARY KEY,
-                      country_name TEXT NOT NULL,
-                      country_code INTEGER,
-                      image_url TEXT,
-                      region_id INTEGER,
-                      FOREIGN KEY (region_id)
-                          REFERENCES regions (region_id)
-               )''')
+    cursor.execute('''CREATE TABLE countries (
+                          country_id INTEGER PRIMARY KEY,
+                          country_name TEXT NOT NULL,
+                          country_code INTEGER,
+                          image_url TEXT,
+                          region_id INTEGER,
+                          FOREIGN KEY (region_id)
+                              REFERENCES regions (region_id)
+                   )''')
 
-connection.commit()
+    cursor.execute('''CREATE TABLE happiness_report (
+                          id INTEGER PRIMARY KEY,
+                          country_id INTEGER,
+                          score REAL,
+                          FOREIGN KEY (country_id)
+                              REFERENCES countries (country_id)
+                   )''')
 
-connection.close()
+    connection.commit()
+    connection.close()
+
+
+if __name__ == '__main__':
+    create_database("out/happiness.db")
+
--- extract_data.py
+++ extract_data.py
@@ -3,35 +3,42 @@
 import json
 import sqlite3
 
-connection = sqlite3.connect('out/happiness.db')
+def extract_countries_info(countries_info_path, db_path):
+    '''Extract countries information from a JSON-formatted file,
+       and store them in the db_path database.
+    '''
+    connection = sqlite3.connect(db_path)
 
-cursor = connection.cursor()
+    cursor = connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
 
-cursor.execute("PRAGMA foreign_keys=ON")
+    with open(countries_info_path, 'r') as f:
+        for country_data in json.load(f):
+            cursor.execute('''INSERT OR IGNORE INTO regions
+                                  (region_name,
+                                   region_code)
+                              VALUES
+                                  (?, ?)''',
+                           (country_data['region'],
+                            country_data['region-code']))
 
-with open('data/countries_continents_codes_flags_url.json', 'r') as f:
-    for country_data in json.load(f):
-        cursor.execute('''INSERT OR IGNORE INTO regions
-                              (region_name,
-                               region_code)
-                          VALUES
-                              (?, ?)''',
-                       (country_data['region'],
-                        country_data['region-code']))
+            cursor.execute('''INSERT INTO countries
+                                  (country_name,
+                                   country_code,
+                                   image_url,
+                                   region_id)
+                              VALUES
+                                  (?, ?, ?,
+                                  (SELECT region_id FROM regions WHERE region_name = ?)
+                                  )''',
+                           (country_data['country'],
+                            country_data['country-code'],
+                            country_data['image_url'],
+                            country_data['region']))
 
-        cursor.execute('''INSERT INTO countries
-                              (country_name,
-                               country_code,
-                               image_url,
-                               region_id)
-                          VALUES
-                              (?, ?, ?,
-                              (SELECT region_id FROM regions WHERE region_name = ?)
-                              )''',
-                       (country_data['country'],
-                        country_data['country-code'],
-                        country_data['image_url'],
-                        country_data['region']))
+    connection.commit()
+    connection.close()
 
-connection.commit()
-connection.close()
+
+if __name__ == '__main__':
+    extract_countries_info('data/countries_continents_codes_flags_url.json', 'out/happiness.db')
